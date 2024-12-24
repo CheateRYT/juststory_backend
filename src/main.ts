e3e70f7139b19b1e1ea35ff229b76a0d406944b0
CheateRYT <cheaterytyt@gmail.com>
2024-12-24 13:15:12 +0300

    feat: add swagger module to main.js

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix("api");
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle("Notes API")
+    .setDescription("The notes API description")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("swagger", app, document);
+
   await app.listen(4202);
 }
 bootstrap();
